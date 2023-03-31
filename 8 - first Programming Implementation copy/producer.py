8c5fd8d5d9d1624246b325bd75647131ea53fde9
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-03-31 08:16:24 -0300

    c

--- 8 - first Programming Implementation copy/producer.py
+++ 8 - first Programming Implementation copy/producer.py
@@ -2,6 +2,7 @@ import pika
 import time
 import random
 
+#Start -> Open conection
 connection_parameters = pika.ConnectionParameters('localhost')
 
 connection = pika.BlockingConnection(connection_parameters)
@@ -9,16 +10,18 @@ connection = pika.BlockingConnection(connection_parameters)
 channel = connection.channel()
 
 channel.queue_declare(queue='letterbox')
+#Finish the open conection step
 
-messageID = 1
 
-while (True):
-    message = f"Sending message: {messageID}"
+messageID = 1 #message id
 
-    channel.basic_publish(exchange='', routing_key='letterbox', body=message)
+while (True): #looping
+    message = f"Sending message: {messageID}" #message itself
 
-    print(f"sent message: {message}")
+    channel.basic_publish(exchange='', routing_key='letterbox', body=message) #message publishing
 
-    time.sleep(random.randint(1, 4))
+    print(f"sent message: {message}") #the message in console
 
-    messageID += 1
+    time.sleep(random.randint(1, 4)) #sleep time
+
+    messageID += 1 #sum 1 in he messaege id
