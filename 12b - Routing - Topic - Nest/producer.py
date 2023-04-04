51e7944a321ad8d7929ec40e87e8527eac45919d
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-04-04 11:33:31 -0300

    c

--- 12b - Routing - Topic - Nest/producer.py
+++ 12b - Routing - Topic - Nest/producer.py
@@ -1,14 +1,16 @@
 import pika
 from pika.exchange_type import ExchangeType
 
+exchangeName = 'MATH_SERVICE'
+
 connection_parameters = pika.ConnectionParameters('localhost')
 connection = pika.BlockingConnection(connection_parameters)
 channel = connection.channel()
 
-channel.exchange_declare(exchange='MATH_SERVICE', exchange_type=ExchangeType.topic)
+channel.exchange_declare(exchange=exchangeName, exchange_type=ExchangeType.topic)
 
 message = 'hello world'
-channel.basic_publish(exchange='MATH_SERVICE',routing_key='hello.world', body=message)
+channel.basic_publish(exchange=exchangeName,routing_key='hello.world', body=message)
 print(message)
 
 connection.close()
