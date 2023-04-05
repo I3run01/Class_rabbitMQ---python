1a63b284a16beec191d286a7bc19d57160123f8c
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-04-05 20:47:02 -0300

    c

--- 12c - Routing - Topic - Nest/producer.py
+++ 12c - Routing - Topic - Nest/producer.py
@@ -4,13 +4,12 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 exchange_name = 'exchangeName'
-routing_key = 'hello.worlds'
-queue_name = 'myQueue'
+routing_key = 'hello.world'
 
 channel.exchange_declare(exchange=exchange_name, exchange_type='topic', durable=True)
 
 message = 'Hello, world!'
-channel.basic_publish( exchange=exchange_name, pattern=routing_key, body=message, )
+channel.basic_publish( exchange=exchange_name, routing_key=routing_key, body=message )
 
 print(f'Sent message: {message}')
 
